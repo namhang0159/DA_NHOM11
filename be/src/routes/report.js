8fec3afd3538264edf7e5160b047c9d789b35072
Trung Nguyen <nguyentrungnguyen110621@gmail.com>
2025-12-19 15:25:56 +0700

    Them chuc nang dang nhap

--- be/src/routes/report.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/ReportController");
-
-router.post("/", controller.create);            
-router.get("/", controller.findAll);            
-router.patch("/:id/status", controller.updateStatus);
-
-module.exports = router;
