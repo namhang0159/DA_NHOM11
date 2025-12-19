11bb05a0d2eb115285f39615bf22be392c24cf93
Trung Nguyen <nguyentrungnguyen110621@gmail.com>
2025-12-19 16:47:51 +0700

    Them chuc nang quan ly don hang

---
+++ fe-portal/data/data.js
@@ -0,0 +1,19 @@
+export const menu = [
+  {
+    title: "Quản lý danh mục",
+    href: "/catalog/manage",
+    list: [],
+  },
+  {
+    title: "Quản lý sản phẩm",
+    href: "/product/manage",
+    list: [],
+  },
+  {
+    title: "Quản lý đơn hàng",
+    href: "/order/manage",
+    list: [],
+  },
+  
+  
+];
