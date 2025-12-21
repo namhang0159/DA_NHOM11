30d319cf09559b973daf7d146f4cd597513a6520
namhang0159 <namhang0159@gmail.com>
2025-12-21 14:43:33 +0700

    fix login'

--- fe-portal/pages/index.jsx
+++ fe-portal/pages/index.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect } from 'react';
-import Router from 'next/router';
+import React, { useEffect } from "react";
+import Router from "next/router";
 
 const HomePage = () => {
-	useEffect(() => { Router.push('/product/manage'); }, []);
+  // useEffect(() => { Router.push('/product/manage'); }, []);
 
-	return (
-		<div className="Home"></div>
-	)
-}
+  return <div className="Home"></div>;
+};
 
 export default HomePage;
