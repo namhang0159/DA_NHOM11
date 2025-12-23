0c44aae8dd8c0c58c3e1caf9435cf17fe43e84e0
Trung Nguyen <nguyentrungnguyen110621@gmail.com>
2025-12-23 07:37:51 +0700

    Them ten

--- fe-portal/pages/catalog/manage.jsx
+++ fe-portal/pages/catalog/manage.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import Header from '@/components/Header'
-import ColourManagement from '@/components/CatalogManagementPage/ColourManagement'
-import SizeManagement from '@/components/CatalogManagementPage/SizeManagement'
-import CategoryManagement from '@/components/CatalogManagementPage/CategoryManagement'
+import Header from "@/components/Header";
+import ColourManagement from "@/components/CatalogManagementPage/ColourManagement";
+import SizeManagement from "@/components/CatalogManagementPage/SizeManagement";
+import CategoryManagement from "@/components/CatalogManagementPage/CategoryManagement";
 
 const CatalogManagementPage = () => {
-
-    return (
-        <div className='catalog-management-page'>
-            <Header title="Quản lý danh mục" />
-            <div className="row">
-                <div className="col-8">
-                    <CategoryManagement />
-                </div>
-                <div className="col-4">
-                    <div>
-                        <ColourManagement />
-                    </div>
-                    <div>
-                        <SizeManagement />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="catalog-management-page">
+      <Header title="Quản lý danh mục Nguyễn Trung Nguyên" />
+      <div className="row">
+        <div className="col-8">
+          <CategoryManagement />
         </div>
-    )
-}
+        <div className="col-4">
+          <div>
+            <ColourManagement />
+          </div>
+          <div>
+            <SizeManagement />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CatalogManagementPage
+export default CatalogManagementPage;
